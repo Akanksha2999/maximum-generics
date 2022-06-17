635b3a7ca8a653c914f7891ccad983e5dc44e3cc
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-17 20:57:37 +0530

    Added: Maximum among three integers , float and strings is printed

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -4,28 +4,9 @@ import java.util.Scanner;
 
 public class Maximum {
 
-    //Main method
-    public static void main(String[] args) {
-        System.out.println("Welcome to maximum string program");
-        //Object created named Maximum
-        Maximum obj = new Maximum();
-        System.out.println("Maximum string among the three string is: " + obj.compareTo());
-    }
-
-    //compareTo method checks the max string among three strings and returns it
-    public String compareTo() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the three strings: ");
-        String x = scanner.nextLine();
-        System.out.println(x);
-
-        String y = scanner.nextLine();
-        System.out.println(y);
-
-        String z = scanner.nextLine();
-        System.out.println(z);
-
-        String max = x;
+    //Generic method which is comparing the given elements and returns maximum of it
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+        T max = x;
         if (y.compareTo(max) > 0) {
             max = y;
         }
@@ -34,5 +15,11 @@ public class Maximum {
 
         }
         return max;
+    }//Main method
+
+    public static void main(String[] args) {
+        System.out.printf("Maximum of %d, %d and %d is %d \n\n", 50, 40, 20, maximum(50, 40, 20));
+        System.out.printf("Maximum of %f, %f and %f is %f \n\n", 3.5, 4.5, 5.5, maximum(3.5, 4.5, 5.5));
+        System.out.printf("Maximum of %s, %s and %s is %s \n\n", "Apple", "Peach", "Banana", maximum("Apple", "Peach", "Banana"));
     }
 }
