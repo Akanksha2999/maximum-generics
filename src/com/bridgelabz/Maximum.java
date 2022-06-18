264632b92007b3c4f7554e91643f69f9cb0f76e1
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-18 08:14:54 +0530

    Added:  More than 3 parameters taken and printed maximum among all integers , float and strings and character

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,32 +1,28 @@
 package com.bridgelabz;
 
 //Generic class
-public class Maximum<T extends Comparable<T>> {
-    T x, y, z;
-
-    public Maximum(T x, T y, T z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
+public class Maximum {
 
     //Generic method which is comparing the given elements and returns maximum of it
-    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z, T a) {
         T max = x;
         if (y.compareTo(max) > 0) {
             max = y;
         }
         if (z.compareTo(max) > 0) {
             max = z;
+        }
+        if (a.compareTo(max) > 0) {
+            max = a;
 
         }
         return max;
     }//Main method
 
     public static void main(String[] args) {
-        System.out.printf("Maximum of %d, %d and %d is %d \n\n", 50, 40, 20, maximum(50, 40, 20));
-        System.out.printf("Maximum of %f, %f and %f is %f \n\n", 3.5, 4.5, 5.5, maximum(3.5, 4.5, 5.5));
-        System.out.printf("Maximum of %s, %s and %s is %s \n\n", "Apple", "Peach", "Banana", maximum("Apple", "Peach", "Banana"));
+        System.out.printf("Maximum of %d, %d, %d and %d is %d \n\n", 50, 40, 20, 10, maximum(50, 40, 20, 10));
+        System.out.printf("Maximum of %f, %f, %f and %f is %f \n\n", 3.5, 4.5, 5.5, 2.5, maximum(3.5, 4.5, 5.5, 2.5));
+        System.out.printf("Maximum of %s, %s %s and %s is %s \n\n", "Apple", "Peach", "Banana", "Watermelon", maximum("Apple", "Peach", "Banana", "Watermelon"));
+        System.out.printf("Maximum of %c, %c %c and %c is %c \n\n", 'a', 'b', 'd', 'c', maximum('a', 'b', 'd', 'c'));
     }
 }
